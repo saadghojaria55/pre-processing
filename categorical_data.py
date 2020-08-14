b5936fba1dffdc8960b88378c5922383a9e94b38
Saad Ghojaria <ghojariasaad@gmail.com>
2020-08-14 19:29:37 +0530

    Update categorical_data.py

--- categorical_data.py
+++ categorical_data.py
@@ -4,7 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import pandas as p
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
